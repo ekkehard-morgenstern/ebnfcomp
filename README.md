666f52e7e3816192a7b20c8784a3043a2f2ab5bd
Ekkehard Morgenstern <ekkehard@ekkehardmorgenstern.de>
2019-11-10 11:08:27 +0100

    concept update

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # ebnfcomp
-EBNF Compiler
 
-This compiler for (a variant of) Niklaus Wirth's Extended Backus-Naur Form translates an EBNF file that specifies a 
-language grammar into a parsing table coded in C, and outputs it to the standard output.
+EBNF Compiler and Parser Generator
 
-To compile it, use "make" from the command line. To run it, simply type "./ebnfcomp <inputfile".
+This compiler for (a variant of) Niklaus Wirth's Extended Backus-Naur Form translates an EBNF file that specifies a language grammar into a parser written in one of several languages (currently planned are C and JavaScript).
+
+To run it, simply type "./ebnfcomp inputfile".
 
 If you specify the "--tree" command line option, a syntax tree of the grammar definition will be printed instead.
-
---
+++ ebnfcomp.js
@@ -0,0 +1,27 @@
+#!/usr/bin/js
+
+/*
+    EBNF Compiler and Parser Generator
+    Copyright (C) 2019  Ekkehard Morgenstern
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact Info:
+    E-Mail: ekkehard@ekkehardmorgenstern.de 
+    Mail: Ekkehard Morgenstern, Mozartstr. 1, 76744 Woerth am Rhein, Germany, Europe
+*/
+
+// TBD
+
+console.log( 'hello world' );
